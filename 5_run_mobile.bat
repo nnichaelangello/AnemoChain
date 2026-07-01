af62069c0de5f027ba91e3599767ec390998f567
Michael Angello Qadosy Riyadi <149357319+nnichaelangello@users.noreply.github.com>
2026-07-01 20:02:32 +0700

    Delete 5_run_mobile.bat

--- 5_run_mobile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Menjalankan Aplikasi Flutter (AnemoChain)
-echo ========================================================
-echo            MEMULAI APLIKASI MOBILE ANEMOCHAIN
-echo ========================================================
-echo Pastikan Tablet Infinix Anda sudah terhubung (USB Debugging aktif)!
-cd Mobile
-flutter run -d 13342154BL000712
-pause
